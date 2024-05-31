dcf2d07434c73d4e30ee9943011f8bec48bab212
Ahmedturaj <ahmedturaj12@gmail.com>
2024-06-01 02:08:00 +0600

    get logOut

--- index.js
+++ index.js
@@ -49,6 +49,22 @@ app.post('/jwt', async (req, res) => {
     }).send({ success: true })
 })
 
+// Logout
+app.get('/logout', async (req, res) => {
+    try {
+        res
+            .clearCookie('token', {
+                maxAge: 0,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            })
+            .send({ success: true })
+        console.log('Logout successful')
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 // ____________________________X_______________________________
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
