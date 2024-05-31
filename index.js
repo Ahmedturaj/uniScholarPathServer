64a22390166e4da7db4f0c64cf781073c3718af6
Ahmedturaj <ahmedturaj12@gmail.com>
2024-06-01 02:06:22 +0600

    added jwt post

--- index.js
+++ index.js
@@ -35,6 +35,21 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+// _____________________________Start________________________
+
+app.post('/jwt', async (req, res) => {
+    const user = req.body
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+    })
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    }).send({ success: true })
+})
+
+// ____________________________X_______________________________
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
