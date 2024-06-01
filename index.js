c1e603d8284a4041218285cafdbe8141910249db
Ahmedturaj <ahmedturaj12@gmail.com>
2024-06-01 13:27:28 +0600

    get all users

--- index.js
+++ index.js
@@ -22,108 +22,112 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-
-// _____________________________Collections_________________
-
-const userCollections = client.db('uniScholarPath').collection('users');
-
-
-// _____________________________Start________________________
-
-app.post('/jwt', async (req, res) => {
-    const user = req.body
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-    })
-    res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    }).send({ success: true })
-})
-
-// Logout
-app.get('/logout', async (req, res) => {
     try {
-        res
-            .clearCookie('token', {
-                maxAge: 0,
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+        // _____________________________Collections_________________
+
+        const userCollections = client.db('uniScholarPath').collection('users');
+
+
+        // _____________________________Start________________________
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true })
+        })
+
+        // Logout
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+                console.log('Logout successful')
+            } catch (err) {
+                res.status(500).send(err)
+            }
+        })
+        // Verify Token Middleware
+        const verifyToken = async (req, res, next) => {
+            const token = req.cookies?.token
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.user = decoded
+                next()
             })
-            .send({ success: true })
-        console.log('Logout successful')
-    } catch (err) {
-        res.status(500).send(err)
-    }
-})
-// Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-    const token = req.cookies?.token
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(err)
-            return res.status(401).send({ message: 'unauthorized access' })
         }
-        req.user = decoded
-        next()
-    })
-}
 
-// verifyAdmin
-const verifyAdmin = async (req, res, next) => {
-    const email = req.user.email;
-    const query = { email: email };
-    const user = await userCollections.findOne(query);
-    const isAdmin = user?.role === 'admin';
-    if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' });
+        // verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.user.email;
+            const query = { email: email };
+            const user = await userCollections.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+        // user api
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const existUser = await userCollections.findOne({ email: users.email });
+            if (existUser) {
+                return res.send({ insertedId: null })
+            }
+            const result = await userCollections.insertOne(users);
+            res.send(result);
+        });
+        // get user
+        app.get('/users', async (req, res) => {
+
+            const result = await userCollections.find().toArray();
+            res.send(result);
+        });
+
+        // ____________________________X_______________________________
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-    next();
-}
-
-// user api
-app.post('/users', async (req, res) => {
-    const users = req.body;
-    const existUser = await userCollections.findOne({ email: users.email });
-    if (existUser) {
-        return res.send({ insertedId: null })
-    }
-    const result = await userCollections.insertOne(users);
-    res.send(result);
-});
-
-
-
-// ____________________________X_______________________________
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('The final assignment will be rock........')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`the post number is ${port}`);
 })
